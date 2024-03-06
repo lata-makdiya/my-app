4b7eb9df671ed99436a56e1f0cbe251de8266ffd
lata-makdiya <latamakadiya123@gmail.com>
2024-03-06 09:00:59 +0530

    learned JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // let lang = 'JS';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <h1>Hello World!, we are programming in {lang}</h1>
+    // <div>
+    //   <div>Div 1</div>
+    //   <div>Div 2</div>
+    // </div>
+
+    <>
+      <label htmlFor="">Name</label>
+      <input type="text" placeholder='Enter Name' className='input-field' id='name' />
+      {/* <img src={logo} alt="" /> */}
+    </>
   );
 }
 
